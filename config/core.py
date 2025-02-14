4644c084ea5ee068f7416b8a8293d8527f069765
pccaneda <cavalcantecaneda@gmail.com>
2025-02-14 19:07:35 -0300

    Removed dotenv and old secret handling

--- config/core.py
+++ config/core.py
@@ -1,9 +1,6 @@
-from dotenv import load_dotenv
 import os 
 import yaml
 
-
-load_dotenv()
 
 def load_config(path: str):
     with open(path, 'r') as file:
@@ -12,10 +9,3 @@ def load_config(path: str):
 
 config = load_config('config/config.yaml')
 tools = load_config('config/tools.yaml')
-
-# Injects environment variables
-config['api_keys'] = {
-    'groq': os.getenv("GROQ_API_KEY"),
-    'pinecone': os.getenv("PINECONE_API_KEY"),
-    'weather': os.getenv("WEATHER_API_KEY")
-}
